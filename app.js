30e8df98441063355a2d18e957ea85f486dabb13
Vincent-Gao-78629 <vgao78629@gmail.com>
2025-09-22 14:03:38 -0400

    im jsing it

---
+++ app.js
@@ -0,0 +1,32 @@
+// How to declare a variable in JavaScript
+
+// let: it can be redefined
+let x = 3;
+x = 4; // this is valid
+
+// const: it cannot be redefined
+const y = 3;
+// const y = 4; // this will give an error
+
+// types of data in JavaScript
+// 1. string
+const name = "John";
+
+// 2. integer
+const age = 16;
+
+// 3. boolean
+const vincentiscool = true;
+
+// 4. null
+// null is the absence of value
+
+// 5. undefined
+// undefined values can be given a value
+
+function add(x, y) {
+  return x + y;
+}
+// functions are input-output machines that can accept arguments and parameters
+
+add(3, 4); // this will return 7
