bf1634637b033e89c7455306ec84aaafb8f01e4b
Vincent-Gao-78629 <vgao78629@gmail.com>
2025-09-29 12:24:04 -0400

    ljkn

---
+++ listarray.js
@@ -0,0 +1,23 @@
+const students = ["alice", "bob", "charlie"];
+
+students.push("david");
+// function printStudentNames(list) {
+//   for (let i = 0; i < list.length; i++) {
+//     console.log(i);
+//     console.log(students[i]);
+//   }
+// }
+
+//students.forEach((student) => console.log(student));
+
+// const found = students.find((student) => student === "bob");
+// console.log(found);
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+// console.log(numbers.filter((number) => number % 2 === 0));
+
+// let number = 10;
+// while (number > 0) {
+//   console.log(number);
+//   number--;
+// }
