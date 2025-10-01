a405f2e18075d055a9b3d5eefb7d3979af0c7af8
Vincent-Gao-78629 <vgao78629@gmail.com>
2025-10-01 12:33:18 -0400

    i can do anything

--- parking.js
+++ parking.js
@@ -1,3 +1,15 @@
-const spaces =5
-CC..C
-.CC..
+const spaces = 5;
+const yesterday = "CC..C";
+const today = "C.C.C";
+
+function samespots(yesterday, today, spaces) {
+  let same = 0;
+  for (let i = 0; i < spaces; i++) {
+    if (yesterday[i] === "C" && today[i] === "C") {
+      same = same + 1;
+    }
+  }
+  console.log(same);
+}
+
+samespots(yesterday, today, spaces);
--- rocket.js
+++ rocket.js
@@ -1,6 +1,15 @@
-const money = 0;
+const P = 1; //paint
+const B = 2; //amount per badge
+const D = 2; //dollar
+
 function rocket(P, B, D) {
-    P % B;
-    B*D += money;
+  let money = 0;
+  let leftover = P % B;
+  let badge = Math.floor(P / B);
+  let earning = badge * D;
+  money += earning;
+  money += leftover;
+  console.log(money);
 }
-rocket(1, 2, 3);
+
+rocket(1000, 2, 3);
