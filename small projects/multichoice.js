39b6608730207e9b060814784c9444155118cbd2
Vincent-Gao-78629 <vgao78629@gmail.com>
2025-10-06 12:09:38 -0400

    sd

--- small projects/multichoice.js
+++ small projects/multichoice.js
@@ -1,3 +1,13 @@
-const qnum = 5
-const studenta = (a, a, d, c, b)
-function grade(qnum, studenta, teachera)
+const qnum = 5;
+const studenta = ("A", "A", "D", "C", "B");
+const teachera = ("A", "A", "D", "C", "B");
+
+function grade(qnum, studenta, teachera) {
+  let correct = 0;
+  for (let i = 0; i < qnum; i++) {
+    if (studenta[i] === teachera[i]);
+    correct += 1;
+  }
+}
+
+console.log(grade(qnum, studenta, teachera));
--- small projects/wand.js
+++ small projects/wand.js
@@ -1,5 +1,7 @@
 const swand = A;
 let fwand = swand;
 let rounds = 3;
+const kill = [a, b, c];
+const killed = [a, b, c];
 
 function wizduel(swand, rounds, kill, killed) {}
